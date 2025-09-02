7c2f6287486bbfa6b0a1c52caa59f2687552587c
HighRain_Panchal <panchalgovind053@gmail.com>
2025-09-02 18:03:33 +0530

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    const db = await mongoose.connect(process.env.MONGODB_URL);
     console.log("Database connected successfully");
+    return db.connections;
   } catch (error) {
     console.log("Error connecting to database:", error);
+    
   }
 }
 module.exports = connectDB;
+
